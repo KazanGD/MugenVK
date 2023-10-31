4caac2c29d7eacbe8adbc9ef572197a564d144a2
victfv <victfv@gmail.com>
2023-10-31 07:34:09 -0300

    updated normal map

--- TriangleMK1/TriangleMK1/main.cpp
+++ TriangleMK1/TriangleMK1/main.cpp
@@ -33,7 +33,7 @@
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
-const std::string MODEL_PATH = "models/viking_room.obj";
+const std::string MODEL_PATH = "models/sphere.obj";
 const std::string TEXTURE_PATH = "images/Grass_albedo.png";
 const std::string NORMAL_PATH = "images/Grass_normal.png";
 const int MAX_FRAMES_IN_FLIGHT = 2;
@@ -1032,8 +1032,9 @@ private:
         float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
 
         UniformBufferObject ubo{};
-        ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-        ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(30.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        ubo.view = glm::lookAt(glm::vec3(-1.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        //ubo.view = glm::rotate(ubo.view, time * glm::radians(30.0f), glm::vec3(0.0f, 1.0f, 1.0f));
         ubo.proj = glm::perspective(glm::radians(45.0f), swapChainExtent.width / (float)swapChainExtent.height, 0.1f, 10.0f);
         ubo.proj[1][1] *= -1;
 
--- TriangleMK1/TriangleMK1/shaders/shader.frag
+++ TriangleMK1/TriangleMK1/shaders/shader.frag
@@ -11,9 +11,9 @@ layout(location = 3) in mat3 TBN;
 
 void main()
 {
-	vec3 sunDir = normalize(vec3(-1,1,1));
+	vec3 sunDir = normalize(vec3(0,-1,0.5));
 	vec3 norm = texture(normSampler, fragTexCoord).rgb;
-	norm = normalize(norm * 2.0 - 1.0);
+	norm = normalize((norm * 2.0 - 1.0));
 	norm = normalize(TBN * norm);
 	vec4 lighting = vec4(vec3(max(0.0, -dot(sunDir, norm))),1.0);
 	outColor = texture(texSampler, fragTexCoord) * lighting;
